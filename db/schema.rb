6d8adbe41ec6eee23ca6433507de84d3765f572f
PMGH <petermccready2@gmail.com>
2024-10-11 20:10:37 +0100

    scaffold Project with name and description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_10_151516) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_11_190730) do
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
