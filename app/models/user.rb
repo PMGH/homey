c1cc23e2f893669a0ed0986a4bd8ba8a685612fc
PMGH <petermccready2@gmail.com>
2024-10-12 22:07:43 +0100

    fix destroy user test by using dependent destroy on user has_many user_projects relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :user_projects 
+  has_many :user_projects, dependent: :destroy
   has_many :projects, through: :user_projects
 
   # Include default devise modules. Others available are:
