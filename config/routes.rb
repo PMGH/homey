5c52de93ada2ad2bbe9650ef13b9c84f81955d00
PMGH <petermccready2@gmail.com>
2024-10-12 22:40:28 +0100

    handle devise authenticated and unauthenticated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,28 @@
 Rails.application.routes.draw do
-  resources :project_statuses
-  resources :comments
-  resources :user_projects
-  resources :projects
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "users#index"
-
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
   }
+
+  devise_scope :user do
+    unauthenticated do
+      root to: 'users/sessions#new', as: 'unauthenticated_user_root'
+    end
+
+    authenticated do
+      root to: "projects#index", as: 'authenticated_user_root'
+
+      resources :project_statuses
+      resources :comments
+      resources :user_projects
+      resources :projects
+      resources :users
+    end
+  end
 end
